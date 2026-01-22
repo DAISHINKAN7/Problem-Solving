c15bb0c44d3b1becbcc2329627307f39f69fd8f6
Kunal Ajgaonkar <kunaljgaonkar31682@gmail.com>
2026-01-22 22:16:59 +0530

    Day 30: Problem Solving

---
+++ taskScheduler.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class taskScheduler{
+    public int leastInterval(char[] tasks, int n){
+        int char_map [] = new int[26];
+        for(char c : tasks){
+            char_map[c - 'A']++;
+        }
+
+        Arrays.sort(char_map);
+        int max_val = char_map[25] - 1;
+        int idle_slots = max_val * n;
+
+        for(int i = 24; i >= 0; i--){
+            idle_slots -= Math.min(char_map[i], max_val);
+        }
+
+        return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
+    }
+    public static void main(String[] args) {
+        taskScheduler ts = new taskScheduler();
+
+        System.out.println(ts.leastInterval(new char[]{'A','A','A','B','B','B'}, 2));
+        System.out.println(ts.leastInterval(new char[]{'A','C','A','B','D','B'}, 1));
+        System.out.println(ts.leastInterval(new char[]{'A','A','A','B','B','B'}, 3));
+    }
+
+}
