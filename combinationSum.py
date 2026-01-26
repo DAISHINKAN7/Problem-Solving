1dd086c45f74d44791a90aa3f363c96e843d6bf0
Kunal Ajgaonkar <kunaljgaonkar31682@gmail.com>
2026-01-26 20:22:46 +0530

    Day 34: Problem Solving

---
+++ combinationSum.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, curr, total):
+            if total == target:
+                res.append(curr.copy())
+                return 
+            
+            if i >= len(candidates) or total > target:
+                return
+            
+            candidates[i]
+
+            curr.append(candidates[i])
+            dfs(i, curr, total + candidates[i])
+            curr.pop()
+            dfs(i + 1, curr, total)
+
+        
+        dfs(0, [], 0)
+        return res
+    
+s = Solution()
+print(s.combinationSum([2,3,6,7], 7))
+print(s.combinationSum([2,3,5], 8))
+print(s.combinationSum([2], 1))
