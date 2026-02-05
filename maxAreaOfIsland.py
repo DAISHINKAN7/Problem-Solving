13dcba5038f49a3a8e2576b594cd647555e05f19
Kunal Ajgaonkar <kunaljgaonkar31682@gmail.com>
2026-02-05 22:38:42 +0530

    Day 42: Problem Solving

---
+++ maxAreaOfIsland.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if(r < 0 or r == rows or c < 0 or c == cols or grid[r][c] == 0 or (r, c) in visit):
+                return 0
+            
+            visit.add((r, c))
+            return(1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1))
+        area = 0
+        for r in range(rows):
+            for c in range(cols):
+                area = max(dfs(r, c), area)
+        return area
+    
+
+s = Solution()
+# Example usage:
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+print(s.maxAreaOfIsland(grid))  # Output: 6
