61e5f96b1361cfeb3ad1dd557f60511e3b7f0c76
Kunal Ajgaonkar <kunaljgaonkar31682@gmail.com>
2026-02-12 20:50:42 +0530

    Day 46: Problem Solving

---
+++ courseSecheduler2.py
@@ -0,0 +1,43 @@
+from typing import List
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq = { c : [] for c in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+
+        output = []
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+            
+            cycle.add(crs)
+
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+                
+            cycle.remove(crs)
+            visit.add(crs)
+
+            output.append(crs)
+            return True
+        
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return []
+        return output
+    
+s = Solution()
+
+print(s.findOrder(numCourses = 2, prerequisites = [[1,0]]))
+print(s.findOrder(numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]))
+print(s.findOrder(numCourses = 0, prerequisites = []))
+
+
+
+            
